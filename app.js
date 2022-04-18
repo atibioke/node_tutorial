f4908cfda19df78f015b9c8480b9f112f1df6941
atibioke <eudoxusganium@gmail.com>
2022-04-18 18:14:51 +0100

    studying node.js

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ const items = [1,[2, [3, [4, [5, [6, [7, [8,]]]]]]]]
 const newItems = _.flattenDeep(items)
 console.log(newItems);
 console.log('Hello people');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
