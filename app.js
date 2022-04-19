62cb3a1ccac1bdfec90af9d0e39100db98f63ef2
atibioke <eudoxusganium@gmail.com>
2022-04-19 11:04:53 +0100

    studying nodejs

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const _ = require('lodash')
+// const _ = require('lodash')
 
-const items = [1,[2, [3, [4, [5, [6, [7, [8,]]]]]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems);
-console.log('Hello people');
+// const items = [1,[2, [3, [4, [5, [6, [7, [8,]]]]]]]]
+// const newItems = _.flattenDeep(items)
+// console.log(newItems);
+// console.log('Hello people');
 
+const { readFile } = require('fs')
+console.log('start a first task');
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if(err){
+        console.log(err);
+        return
+    }
+    console.log(result);
+    console.log('complete first task');
+})
+console.log('starting next task');
 
 
 
