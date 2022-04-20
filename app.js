c3d1cf2f91d60e85dec9f2265a8688cff3f47911
atibioke <eudoxusganium@gmail.com>
2022-04-20 11:19:36 +0100

    studying nodejs

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-// const _ = require('lodash')
+const { readFile } = require("fs");
 
-// const items = [1,[2, [3, [4, [5, [6, [7, [8,]]]]]]]]
-// const newItems = _.flattenDeep(items)
-// console.log(newItems);
-// console.log('Hello people');
+function getText(path)  {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
 
-const { readFile } = require('fs')
-console.log('start a first task');
-readFile('./content/first.txt', 'utf8', (err, result) => {
-    if(err){
-        console.log(err);
-        return
+function start(){
+    return async () => {
+        const first = await getText('./content/first.txt')
+        console.log(first);
     }
-    console.log(result);
-    console.log('complete first task');
-})
-console.log('starting next task');
+}
 
+start();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// getText("./content/first.txt")
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
