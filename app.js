d560c19fb41c380956fc6acf6462104fd9ad2f34
atibioke <eudoxusganium@gmail.com>
2022-04-21 11:36:45 +0100

    studying nodejs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-let p = new promise((resolve, reject) => {
+let p = new Promise((resolve, reject) => {
     let a = 1+1;
     if(a == 2){
         resolve('success')
@@ -8,7 +8,7 @@ let p = new promise((resolve, reject) => {
     }
 })
 
-
+p.then((message) => console.log('This is a resolve ' + message + ' message')).catch((message) => console.log('This is a reject ' + message))
 
 
 // const { readFile, writeFile } = require("fs");
