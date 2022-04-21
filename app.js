0fcdcf3c9b1362148c6928735832624060587cd0
atibioke <eudoxusganium@gmail.com>
2022-04-21 15:43:11 +0100

    studying nodejs

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 
 
-let p = new Promise((resolve, reject) => {
-    let a = 1 + 1;
-    if (a == 2) {
-        resolve('success')
-    } else {
-        reject('failed')
-    }
-})
 
-const asyncSample = async () => {
-    try {
-        const result = await p;
-        const result1 = await new Promise((resolve) => setTimeout(() => resolve('1')))
-        const result2 = await new Promise((resolve) => setTimeout(() => resolve('2')))
-        console.log(result);
-        console.log(result1);
-        console.log(result2);
-    } catch (error) {
-        console.log(error)
-    }
-}
 
-asyncSample()
+
+
+
+
+// let p = new Promise((resolve, reject) => {
+//     let a = 10;
+//     if (a > 11) {
+//         resolve('success')
+//     } else {
+//         reject('failed')
+//     }
+// })
+
+// const asyncSample = async () => {
+//     try {
+//         const result = await p;
+//         const result1 = await new Promise((resolve) => setTimeout(() => resolve('1')))
+//         const result2 = await new Promise((resolve) => setTimeout(() => resolve('2')))
+//         console.log(result);
+//         console.log(result1);
+//         console.log(result2);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// asyncSample()
 
 
 
@@ -46,25 +52,7 @@ asyncSample()
 
 
 
-// const { readFile, writeFile } = require("fs");
-// const util = require("util");
-// const readFilePromise = util.promisify(readFile); 
-// const writeFilePromise = util.promisify(writeFile); 
 
-
-// const start = async () => {
-//     try {
-//       const first = await readFilePromise("./content/first.txt", 'utf8');
-//       const second = await readFilePromise("./content/second.txt", 'utf8');
-//       await writeFilePromise("./content/result-mind-grenade.txt", `THIS IS AWESOME : ${first} ${second}`); 
-  
-//       console.log(first, second);
-//     } catch (error) {
-//       console.log(first);
-//     }
-//   };
-   
-//   start();
 
 
 
