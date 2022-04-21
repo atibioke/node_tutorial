bd1c12bcbd6344fd27af8bc12cf3640b64330696
atibioke <eudoxusganium@gmail.com>
2022-04-21 11:27:38 +0100

    studying nodejs

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-const { readFile, writeFile } = require("fs");
-const util = require("util");
-const readFilePromise = util.promisify(readFile); 
-const writeFilePromise = util.promisify(writeFile); 
 
-
-const start = async () => {
-    try {
-      const first = await readFilePromise("./content/first.txt", 'utf8');
-      const second = await readFilePromise("./content/second.txt", 'utf8');
-      await writeFilePromise("./content/result-mind-grenade.txt", `THIS IS AWESOME : ${first} ${second}`); 
-  
-      console.log(first, second);
-    } catch (error) {
-      console.log(first);
+let p = new promise((resolve, reject) => {
+    let a = 1+1;
+    if(a == 2){
+        resolve('success')
+    }else{
+        reject('failed')
     }
-  };
+})
+
+
+
+
+// const { readFile, writeFile } = require("fs");
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile); 
+// const writeFilePromise = util.promisify(writeFile); 
+
+
+// const start = async () => {
+//     try {
+//       const first = await readFilePromise("./content/first.txt", 'utf8');
+//       const second = await readFilePromise("./content/second.txt", 'utf8');
+//       await writeFilePromise("./content/result-mind-grenade.txt", `THIS IS AWESOME : ${first} ${second}`); 
+  
+//       console.log(first, second);
+//     } catch (error) {
+//       console.log(first);
+//     }
+//   };
    
-  start();
+//   start();
+
+
+
+
+
+
+
+
 
 // function getText(path) {
 //   return new Promise((resolve, reject) => {
