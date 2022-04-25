a9e457a72bceabc7ee48b5a6f6ca118f9e41cde8
atibioke <eudoxusganium@gmail.com>
2022-04-25 12:53:12 +0100

    studying nodejs

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ console.log(eventBox);
 
 const customEmitter = new eventBox()
 
+customEmitter.on('response', ( name, id)=>{
+    console.log(`data recieved by user ${name} with id: ${id}`);
+})
 customEmitter.on('response', ()=>{
-    console.log(`data recieved`);
+    console.log(`some other logic code`);
 })
 
-customEmitter.emit('response')
+customEmitter.emit('response', 'rasgan', 28)
 
 
 
